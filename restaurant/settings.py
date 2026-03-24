6df3686687cea7baa1d7fb354069a61f637140bc
Abhishekpyla <abhis141654u@gmail.com>
2026-03-24 11:18:45 +0530

    fixed production database config

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -76,14 +76,13 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restaurant',
-        'USER': 'postgres',
-        'PASSWORD': '127110',
-        'HOST': 'localhost'
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
